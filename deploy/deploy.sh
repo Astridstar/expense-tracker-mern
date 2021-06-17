dd7185fa2142195e7fc8436b6143228a84a31f4d
Sarah <sohsum@gmail.com>
2021-06-16 22:39:43 -0700

    Dockerize both react app and nodejs server for development

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,9 @@
 echo "Create mongodb database directories"
 mkdir -pv /home/$(whoami)/mongodb/database
 
-echo "Run docker-compose"
-docker-compose up -d
+#echo "Run docker-compose"
+#docker-compose -f docker-compose-mongodb.yml up -d --force-recreate --no-build
+#docker-compose up -d --force-recreate --no-build
+
+echo "Build and run docker-compose"
+docker-compose up --build -d
